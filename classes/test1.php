443c21996d823a173ad464f91d432897771273e4
ww <ww@ww-PC>
2018-12-19 01:49:21 +0200

    finito

--- classes/test1.php
+++ classes/test1.php
@@ -2,8 +2,8 @@
 
 $params['title'] = 'The Title';
 $params['number'] = '1000';
-// getcwd().
-$html = parse('../templates/test1.tpl', 'section1', $params);
+
+$html = parse('templates/test1.tpl', 'section1', $params);
 
 echo $html;
 
--- parser.php
+++ parser.php
@@ -13,10 +13,10 @@ function parse($file, $section, $vars = null) {
         
         $fileLines = file($file);
         $file_i = implode('<@>', $fileLines);
-        $start_i = strpos($file_i,'['.$section.']');
-        $start = sizeof(explode('<@>', substr($file_i,0,$start_i))) - 1;
-        $end_i = strpos($file_i,'[/'.$section.']');
-        $end = sizeof(explode('<@>', substr($file_i,0,$end_i))) - 1;
+        $start_i = strpos($file_i, '['.$section.']');
+        $start = sizeof(explode('<@>', substr($file_i, 0, $start_i))) - 1;
+        $end_i = strpos($file_i, '[/'.$section.']');
+        $end = sizeof(explode('<@>', substr($file_i, 0, $end_i))) - 1;
 
 
         for ($i=$start+1; $i<=$end-1; $i++) {
@@ -32,13 +32,13 @@ function parse($file, $section, $vars = null) {
         $result = preg_replace('!\[comment\].+\[/comment\]!s', '', $result);
 
         if (is_array($vars)) {
-                foreach($vars as $key => $value) {
+                foreach($vars as $key => $value) { 
                         $tag = "{VAR:" . $key . "}";
-                        $source = str_replace($tag,$value,$result);
+                        $result = str_replace($tag, $value, $result);
                 }
         }
 
-        $result = preg_replace ("/{VAR:(.*)}/U", "", $result);
+        $result = preg_replace("/{VAR:(.*)}/U", "", $result);
 
         return $result;
 }
