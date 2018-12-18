e6ec1dea789f64d565c730d3cd0b27880c5b1c45
ww <ww@ww-PC>
2018-12-19 01:28:54 +0200

    tpl parsimine lisatud

---
+++ parser.php
@@ -0,0 +1,45 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function parse($file, $section, $vars = null) {
+		
+        clearstatcache();
+        
+        if (isset($GLOBALS['parser'][$section])) return $GLOBALS['parser'][basename($file)][$section];
+        
+        $fileLines = file($file);
+        $file_i = implode('<@>', $fileLines);
+        $start_i = strpos($file_i,'['.$section.']');
+        $start = sizeof(explode('<@>', substr($file_i,0,$start_i))) - 1;
+        $end_i = strpos($file_i,'[/'.$section.']');
+        $end = sizeof(explode('<@>', substr($file_i,0,$end_i))) - 1;
+
+
+        for ($i=$start+1; $i<=$end-1; $i++) {
+                $result .= $fileLines[$i];
+        }
+        
+        if (empty($result)) {
+                return 'source: '.$file. ' is empty ('.$section.')';
+        }
+
+        $GLOBALS['parser'][basename($file)][$section] = $result;
+
+        $result = preg_replace('!\[comment\].+\[/comment\]!s', '', $result);
+
+        if (is_array($vars)) {
+                foreach($vars as $key => $value) {
+                        $tag = "{VAR:" . $key . "}";
+                        $source = str_replace($tag,$value,$result);
+                }
+        }
+
+        $result = preg_replace ("/{VAR:(.*)}/U", "", $result);
+
+        return $result;
+}
+
---
+++ templates/test1.tpl
@@ -0,0 +1,4 @@
+[section1]
+<h1>{VAR:title}</h1>
+The number is: "{VAR:number}"
+[/section1]
